7dc9614349154f2cf1c62b0db6aa7f30492d0696
Kazuya Izumi <shikibu9419@gmail.com>
2024-09-27 07:14:18 +0900

    🎸 feat: [chrome] add Readable button to list

--- Chrome-Extension/scripts/top/document_idle.ts
+++ Chrome-Extension/scripts/top/document_idle.ts
@@ -1,24 +1,44 @@
 import { waitQuerySelector } from "../utils";
 
-function appendNoteButton(element: Element) {
-  const nextButton = element.cloneNode();
-  nextButton.textContent = "Note";
-  nextButton.addEventListener("mousedown", (e) => {
-    alert("not implemented");
-  });
+type ButtonFunctionArgs = { event: Event; item: Element };
 
-  element.insertAdjacentElement("beforebegin", nextButton as Element);
+function appendReadablePDF(item: Element) {
+  const title = item.querySelector("span.pp-grid-titletext")?.textContent;
+  console.log(title, import.meta.env.VITE_READABLE_API_URL);
+  alert("not implemented");
+  // fetch(READABLE_API_URL, { method: "POST", body: JSON.stringify({ title }) });
 }
 
-const selector = "div.referenceAnimation";
-const observer = new MutationObserver((mutationsList, observer) => {
-  console.log("new mutation:", mutationsList);
+// TODO: ユーザが設定可能に
+const buttons: [[string, (args: ButtonFunctionArgs) => void]] = [
+  ["Readable", ({ item }) => appendReadablePDF(item)],
+];
+
+const REFERENCE_ITEM_SELECTOR = "div.referenceAnimation";
+const REFERENCE_ITEM_BUTTON_SELECTOR = "button.referencePdfButton";
+
+function appendButtons(referenceItem: Element) {
+  buttons.forEach(([label, action]) => {
+    const existsButton = referenceItem.querySelector<HTMLButtonElement>(
+      REFERENCE_ITEM_BUTTON_SELECTOR
+    );
+    if (!existsButton) return;
+
+    const newButton = document.createElement("button");
+    newButton.classList.add("referencePdfButton");
+    newButton.textContent = label;
+    newButton.addEventListener("mousedown", (event) => action({ event, item: referenceItem }));
+
+    existsButton.insertAdjacentElement("beforebegin", newButton as Element);
+  });
+}
+
+const observer = new MutationObserver((mutationsList) => {
   mutationsList.forEach((mutation) => {
     if (mutation.type === "childList") {
       mutation.addedNodes.forEach((node) => {
-        if (node instanceof Element && node.matches(selector)) {
-          const button = node.querySelector<HTMLButtonElement>("button.referencePdfButton");
-          if (button) appendNoteButton(button);
+        if (node instanceof Element && node.matches(REFERENCE_ITEM_SELECTOR)) {
+          appendButtons(node);
         }
       });
     }
@@ -29,8 +49,8 @@ async function initNoteButton() {
   const root = await waitQuerySelector(".ReactVirtualized__Grid__innerScrollContainer");
 
   if (root) {
-    root.querySelectorAll<HTMLButtonElement>("button.referencePdfButton").forEach((pdfButton) => {
-      appendNoteButton(pdfButton);
+    root.querySelectorAll<HTMLButtonElement>(REFERENCE_ITEM_SELECTOR).forEach((item) => {
+      appendButtons(item);
     });
 
     observer.observe(root, { childList: true });
