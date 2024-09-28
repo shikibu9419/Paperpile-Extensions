d978c6ac4f0c2d65c131224f9358a0676648f76b
Kazuya Izumi <shikibu9419@gmail.com>
2024-09-28 09:43:46 +0900

    🐛 fix: Raycast actions

---
+++ Raycast/raycast-env.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `index` command */
+  export type Index = ExtensionPreferences & {
+  /** BibTeX Path - paperpile.bib location */
+  "bibtexPath": string
+}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `index` command */
+  export type Index = {}
+}
+
+
+
--- Raycast/src/index.tsx
+++ Raycast/src/index.tsx
@@ -1,9 +1,11 @@
 import { useCallback, useState } from "react";
-import { Color, getPreferenceValues, Icon, List, PreferenceValues } from "@raycast/api";
-import fs from 'fs';
+import { Action, ActionPanel, Color, getPreferenceValues, Icon, List, PreferenceValues } from "@raycast/api";
+import { useCachedState } from "@raycast/utils";
+import fs from "fs";
+import FuzzySearch from "fuzzy-search";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import * as bibtexParse from 'bibtex-parse';
-import FuzzySearch from 'fuzzy-search';
+import * as bibtexParse from "bibtex-parse";
 
 interface BibItem {
   key: string; // cite key
@@ -20,55 +22,71 @@ interface BibItem {
 
 const { bibtexPath }: PreferenceValues = getPreferenceValues();
 
-const bibtex = fs.readFileSync(bibtexPath, 'utf8');
+const bibtex = fs.readFileSync(bibtexPath, "utf8");
 
+const trimString = (str: string) => str.replace(/\s+/g, " ").trim();
 
-const trimString = (str: string) => str.replace(/\s+/g, ' ').trim();
-
-const items: BibItem[] = bibtexParse.entries(bibtex).map((item: any) => ({
+const allItems: BibItem[] = bibtexParse.entries(bibtex).map((item: any) => ({
   ...item,
-  TITLE: trimString(item.TITLE || ''),
-  AUTHOR: trimString(item.AUTHOR || ''),
-  ABSTRACT: trimString(item.ABSTRACT || ''),
-  KEYWORDS: item.KEYWORDS?.split(',')?.map((k: string) => k.trim()) || [],
-}))
+  TITLE: trimString(item.TITLE || ""),
+  AUTHOR: trimString(item.AUTHOR || ""),
+  ABSTRACT: trimString(item.ABSTRACT || ""),
+  KEYWORDS: item.KEYWORDS?.split(",")?.map((k: string) => k.trim()) || [],
+}));
 
-const searcher = new FuzzySearch(items, ['TITLE', 'AUTHOR', 'KEYWORDS', 'ABSTRACT'], {
+const searcher = new FuzzySearch(allItems, ["TITLE", "AUTHOR", "KEYWORDS", "ABSTRACT"], {
   caseSensitive: false,
   sort: true,
 });
 
 export default function Command() {
-  const [filteredItems, setFilteredItems] = useState(items);
+  const [showDetails, setShowDetails] = useCachedState("show-details", true);
+  const [items, setItems] = useState(allItems);
 
-  const handleTextChange = useCallback((text: string) => {
-    setFilteredItems(searcher.search(text));
-  }, [searcher, items])
+  const handleTextChange = useCallback(
+    (text: string) => {
+      setItems(searcher.search(text));
+    },
+    [searcher, allItems]
+  );
 
   return (
-    <List isShowingDetail onSearchTextChange={handleTextChange}>
-      {filteredItems.map((item, index) => (
+    <List isShowingDetail={showDetails} onSearchTextChange={handleTextChange}>
+      {items.map((item) => (
         <List.Item
-          key={index}
+          key={item.key}
           icon={{ source: Icon.Document, tintColor: Color.Green }}
           title={item.TITLE}
           subtitle={item.AUTHOR}
           keywords={item.KEYWORDS}
-          detail={<List.Item.Detail markdown={item.ABSTRACT} metadata={
-            <List.Item.Detail.Metadata>
-            <List.Item.Detail.Metadata.Label title="Title" text={item.TITLE} />
-            <List.Item.Detail.Metadata.Label title="Author" text={item.AUTHOR} />
-            <List.Item.Detail.Metadata.Label title="Journal" text={item.JOURNAL} />
-            <List.Item.Detail.Metadata.Label title="DOI" text={item.DOI} />
-            </List.Item.Detail.Metadata>
-          } />}
-//           actions={
-//             <ActionPanel>
-//               <Action.Open title="Open file" target={`${targetDir}/${item.path}`} />
-//               <Action.CopyToClipboard title="Copy to Clipboard" content={`${targetDir}/${item.path}`} />
-//               <Action.Push title="Help" target={<Help />} />
-//             </ActionPanel>
-//           }
+          detail={
+            <List.Item.Detail
+              markdown={item.ABSTRACT}
+              metadata={
+                <List.Item.Detail.Metadata>
+                  <List.Item.Detail.Metadata.Label title="Title" text={item.TITLE} />
+                  <List.Item.Detail.Metadata.Label title="Author" text={item.AUTHOR} />
+                  <List.Item.Detail.Metadata.Label title="Journal" text={item.JOURNAL} />
+                  <List.Item.Detail.Metadata.Label title="DOI" text={item.DOI} />
+                </List.Item.Detail.Metadata>
+              }
+            />
+          }
+          actions={
+            <ActionPanel>
+              <Action.OpenInBrowser
+                title="Open in Paperpile"
+                url={`https://app.paperpile.com/my-library/all?title=${item.TITLE}`}
+              />
+              {item.URL && <Action.OpenInBrowser title="Open DOI" url={item.URL} />}
+              <Action
+                icon={Icon.AppWindowSidebarRight}
+                shortcut={{ modifiers: ["cmd", "shift"], key: "d" }}
+                title={showDetails ? "Hide Details" : "Show Details"}
+                onAction={() => setShowDetails((value) => !value)}
+              />
+            </ActionPanel>
+          }
         />
       ))}
     </List>
